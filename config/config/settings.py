d3c76789bc48d9fdb2a7731fb8b70a336300df3d
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-01-23 10:36:21 +0300

    Поменял название главной страницы

--- config/config/settings.py
+++ config/config/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'posts.apps.PostsConfig',
+    'main_page.apps.PostsConfig',
 
 
 ]
@@ -118,3 +118,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# print ("base dir path", BASE_DIR)
